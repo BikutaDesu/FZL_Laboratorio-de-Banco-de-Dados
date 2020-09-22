c3b8f924c6169e618eab060ee0a4001b590e2e8f
BikutaDesu <victor.neves4@fatec.sp.gov.br>
2020-09-22 16:56:25 -0300

    aula_21-09-20

---
+++ Exercicios/aula_21-09-20/src/main/java/br/com/exercicio/dao/GenericDAO.java
@@ -0,0 +1,23 @@
+package br.com.exercicio.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GenericDAO {
+
+    private Connection c;
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+
+        String hostName = "localhost";
+        String dbName ="videoaulaproc";
+        String user ="sa";
+        String senha ="Neves@123";
+        Class.forName("net.sourceforge.jtds.jdbc.Driver");
+        c = DriverManager.getConnection(String.format("jdbc:jtds:sqlserver://%s:1433;databaseName=%s;user=%s;password=%s;", hostName, dbName, user, senha));
+
+        return c;
+    }
+
+}
